773f9c1e12017fecba8d04edd3481ce38471291c
Erik Svensson <eosvensson@gmail.com>
2019-09-27 11:24:26 +0200

    Added Product

---
+++ src/main/java/se/lexicon/erik/jpa_workshop/entity/Product.java
@@ -0,0 +1,73 @@
+package se.lexicon.erik.jpa_workshop.entity;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+public class Product {
+	
+	private int productId;
+	private String productName;
+	private BigDecimal price;
+	
+	public Product(int productId, String productName, BigDecimal price) {
+		this.productId = productId;
+		setProductName(productName);
+		setPrice(price);
+	}
+
+	public Product(String productName, BigDecimal price) {
+		this(0, productName, price);
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public BigDecimal getPrice() {
+		BigDecimal returnValue = price.setScale(2);
+		return returnValue;
+	}
+
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+
+	public int getProductId() {
+		return productId;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(price, productId, productName);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		return Objects.equals(price, other.price) && productId == other.productId
+				&& Objects.equals(productName, other.productName);
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Product [productId=");
+		builder.append(productId);
+		builder.append(", productName=");
+		builder.append(productName);
+		builder.append(", price=");
+		builder.append(getPrice());
+		builder.append("]");
+		return builder.toString();
+	}	
+}
---
+++ src/test/java/se/lexicon/erik/jpa_workshop/entity/ProductTest.java
@@ -0,0 +1,43 @@
+package se.lexicon.erik.jpa_workshop.entity;
+
+import java.math.BigDecimal;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ProductTest {
+	
+	private Product testObject;
+	
+	@Before
+	public void setup() {
+		testObject = new Product(1, "Spam", BigDecimal.valueOf(10.90));
+	}
+	
+	@Test
+	public void testObject_successfully_created() {
+		assertEquals(1, testObject.getProductId());
+		assertEquals("Spam", testObject.getProductName());
+		assertEquals(BigDecimal.valueOf(10.90).setScale(2), testObject.getPrice());
+	}
+	
+	@Test
+	public void test_equals_and_hashCode() {
+		Product copy = new Product(1, "Spam", BigDecimal.valueOf(10.90));
+		assertTrue(copy.equals(testObject));
+		assertEquals(copy.hashCode(), testObject.hashCode());
+	}
+	
+	@Test
+	public void test_toString_contains_correct_information() {
+		String toString = testObject.toString();
+		assertTrue(
+				toString.contains("1") &&
+				toString.contains("Spam") &&
+				toString.contains("10.90")
+		);		
+	}
+
+}
