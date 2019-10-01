2525a6ad6c7a44f08816bbaf400671b067298f6e
Erik Svensson <eosvensson@gmail.com>
2019-10-01 15:08:10 +0200

    Organized imports in ProductOrder

--- src/main/java/se/lexicon/erik/jpa_workshop/entity/ProductOrder.java
+++ src/main/java/se/lexicon/erik/jpa_workshop/entity/ProductOrder.java
@@ -4,12 +4,8 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
-import java.util.Set;
-import java.util.TreeSet;
-import java.util.stream.Collectors;
 
 
 public class ProductOrder {
